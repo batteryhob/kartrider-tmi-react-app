af0db533f71734b24ea2b228133ed2fa486d6893
batteryho <gorokke18@gmail.com>
2020-06-08 21:18:31 +0900

    테스트코드 세팅

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,9 +1,35 @@
 import React from 'react';
 
-const Header = () => (
-    <div className="header">
-        헤더
-    </div>
-)
+const Header = ({ menu1, menu2, menu3, menu4 }) => {
+  return (
+    <header>
+        <div className="contents">
+            <i className="fas fa-home"></i>
+            <div className="menu-center desktop">
+                <ul>
+                    <li className="active">
+                     { menu1 }
+                    </li>
+                    <li>
+                     { menu2 }
+                    </li>
+                    <li>
+                     { menu3 }
+                    </li>
+                    <li>
+                     { menu4 }
+                    </li>
+                </ul>
+                <div className="underbar">
+                </div>
+            </div>
+            <div className="menu-right">
+                <i className="fas fa-search"></i>
+                <i className="fas fa-bars"></i>
+            </div>
+        </div>
+    </header>
+  );
+}
 
 export default Header;
---
+++ src/shared/Header.test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {render, fireEvent, screen} from '@testing-library/react'
+import '@testing-library/jest-dom'
+import Header from './Header';
+
+describe('<Header />', () => {
+  it('shows the props correctly', () => {
+    const utils = render(<Header menu1="검색" menu2="랭킹" menu3="카트" menu4="트랙" />);
+    utils.getByText('검색');
+    utils.getByText('랭킹');
+  });
+});
